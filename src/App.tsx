a6598639e50ecaff29e122b6c5ede0f249f3e6d9
Ejaz <ayanejaz.fistix@gmail.com>
2024-04-30 12:17:21 +0500

    diceRoll

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { DiceRoll } from './DiceRoll'
+
+
+export default function App() {
+  return (
+    <View>
+      <DiceRoll/>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
